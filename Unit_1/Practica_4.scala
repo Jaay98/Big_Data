8d84d55af50c30642b4204dea23407540cd764de
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2020-10-21 22:06:07 -0700

    Update Practica_4.scala

--- Unit_1/Practica_4.scala
+++ Unit_1/Practica_4.scala
@@ -1,3 +1,34 @@
 //Practice 4 
 //Author Alvarez Yanez Jose Alonso
 //17210526
+
+//Make a code who runs a Fibbonacci Series
+
+//Algorithm 1
+
+def fibbo1(numero:Int) : Int ={
+
+    if(numero<2)
+        {
+            return numero
+        }
+        else 
+        {
+            return fibbo1 (numero-1) + fibbo1 (numero-2)
+        }
+}
+//Algorithm 2
+
+def fibbo2(numero:Int) : Int ={
+
+    if(numero<2) {
+        return numero
+    }
+    else {
+        
+    }
+
+}
+//Algorithm 3
+//Algorithm 4
+//Algorithm 5
