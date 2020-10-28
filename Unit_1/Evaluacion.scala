02e36d4cec59e3a87a2fd3d53b5cf4e40f9fe064
Alonso Yanez <60414201+Jaay98@users.noreply.github.com>
2020-10-27 21:56:10 -0700

    Update_Evaluacion_Alonso

--- Unit_1/Evaluacion.scala
+++ Unit_1/Evaluacion.scala
@@ -3,65 +3,63 @@
 //Materia: Datos Masivos
 //Evaluacion Unidad 1
 
-//1-Comienza una simple sesión Spark
-import org.apache.spark.sql.SparkSession
+/*The first step is to import the library to create an entry point 
+to sqlspark and create a session in spark*/
+import org.apache.spark.sql.SparkSession 
 val spark = SparkSession.builder().getOrCreate()
 
-//2-.Cargue el archivo Netflix CSV,haga que Spark infiera los tipos de datos
-val df = spark.read.option("header","true").option("inferSchema","true").csv("/Users/alons/OneDrive/Escritorio/Universidad/Datos Masivos/Big_Data/Unit_1/*.csv")
+//2-. The next step is to load the Netflix CSV file, with the dataframe df and spark infer the data types
+val df = spark.read.option("header","true").option("inferSchema","true").csv("Netflix_2011_2016.csv")
 
-
-//3-.Cuáles son los nombres de las columnas
+//3-.In the following instruction it is in charge of showing us the columns with our df followed by the word column
 df.columns
 
-//4-.Cómo es el esquema
-df.printSchema()
+//4-.It shows us the scheme that are the characteristics of the columns
+df.printSchema() 
 
-//5-.Imprime las primeras 5 columnas
-df.head(5)
+//5-. It takes care of printing the first 5 columns
+df.head(5) 
 
-//6-.Usa describe() para aprender sobre el DataFrame
-df.describe().show()
+//6-.The describes ()returns the basic metadata information of the df
+df.describe().show() 
 
-/*7-.Crea un nuevo dataframe con una columna nueva llamada “HV Ratio” que es la  relación entre el precio 
-de la columna “High” frente a la columna “Volume” de  acciones negociadas por un día. 
-(Hint: Es una operación de columnas)*/
+/*7.- Create a new dataframe whit a new column named "HV_Ratio" who is the relation of the price of "high" columns
+and "volume" of action negotiated by day */
 val df_hv = df.withColumn("HV Ratio",df("High")/df("Volume"))
 df_hv.columns
 
-// 8. ¿Qué día tuvo el pico mas alto en la columna “Close”? 
+//8.- What day had the highest peak in the “Close” column?
 df.orderBy($"High".desc).show(1)
 
-/*9. Escribe con tus propias palabras en un comentario de tu codigo. ¿Cuál es el  significado de la columna
- Cerrar “Close”?*/
+ //9.- Who is the mean of the column "close" ?
+
+ //Answer: This column is the average between the columns "open", "high" and "low"
 df.select(mean("Close")).show()
 
-// 10. ¿Cuál es el máximo y mínimo de la columna “Volume”?
+// 10. What is the maximum and minimum of the “Volume” column?
 df.select(max("Volume")).show()
 df.select(min("Volume")).show()
 
 //11.
-
-//Sintaxis Scala/Spark
+// Sintaxis Scala/Spark
 import spark.implicits._
 
-// a. ¿Cuántos días fue la columna “Close” inferior a $ 600? 
+//a. How many days the column "close" was lowe tha $600?
 df.filter($"Close"<600).count()
 
+//b. Who percentage of the time was the column "High" higher than $500?
+(df.filter($"High" > 500).count() 
 
-//b. ¿Qué porcentaje del tiempo fue la columna “High” mayor que $ 500?
-(df.filter($"High" > 500).count() //* 1.0/ df.count())*100
-
-// c. ¿Cuál es la correlación de Pearson entre columna “High” y la columna “Volumen”?
+// c. Find the Pearson correlation of the colums "high" and "volume"
 df.select(corr("High","Volume")).show()
 
-//  d. ¿Cuál es el máximo de la columna “High” por año? 
+//d. Who was the maximun in the column "high" by year?
 val yeardataframe = df.withColumn("Year",year(df("Date")))
 val yearmaxs = yeardataframe.select($"Year",$"High").groupBy("Year").max()
 val res = yearmaxs.select($"Year",$"max(High)")
 res.orderBy("Year").show()
 
-// e. ¿Cuál es el promedio de columna “Close” para cada mes del calendario? 
+//e. Find the average of the column "close" for each month of the year
 val monthdataframe = df.withColumn("Month",month(df("Date")))
-val monthavg = monthdataframe.select($"Month",$"Close").groupBy("Month").mean()
-monthavg.select($"Month",$"avg(Close)").orderBy("Month").show()
+val monthavgs = monthdataframe.select($"Month",$"Close").groupBy("Month").mean()
+monthavgs.select($"Month",$"avg(Close)").orderBy("Month").show()
